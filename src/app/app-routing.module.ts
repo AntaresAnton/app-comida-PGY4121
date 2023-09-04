138beea0bf265426fb080fd13c81087526e7bf27
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-09-04 15:38:12 -0300

    seccion seleccionar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'seleccionar',
+    loadChildren: () => import('./seleccionar/seleccionar.module').then( m => m.SeleccionarPageModule)
+  },
 ];
 
 @NgModule({
