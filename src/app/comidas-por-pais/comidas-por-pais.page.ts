53c8169eb6425833be5d3efd34eebe6dc0e006df
Patrick <patricioquintanilladx@gmail.com>
2023-11-04 10:56:56 -0300

    .

--- src/app/comidas-por-pais/comidas-por-pais.page.ts
+++ src/app/comidas-por-pais/comidas-por-pais.page.ts
@@ -4,6 +4,20 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-comidas-por-pais',
   templateUrl: './comidas-por-pais.page.html',
   styleUrls: ['./comidas-por-pais.page.scss'],
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Canadian Cuisine</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item *ngFor="let dish of dishes">
+          <ion-label>{{ dish.strMeal }}</ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  `
 })
 export class ComidasPorPaisPage implements OnInit {
 
